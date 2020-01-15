141b7b2d5688ec9e3fc9d09c00b0086a51b7ee43
Leonardo Cardozo Ferreira <leonardo329cf@gmail.com>
2020-01-15 17:00:46 -0300

    Search by techs implemented

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,16 @@
+const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
+
+module.exports = {
+    async index(request, response) {
+        const { latitude, longitude, techs } = request.query
+        const techsArray = parseStringAsArray(techs)
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+        })
+        return response.json({ devs })
+    }
+}
