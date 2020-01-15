42f191bb8b58387c70b44d6d56c9a08fefb45be1
Leonardo Cardozo Ferreira <leonardo329cf@gmail.com>
2020-01-15 17:07:57 -0300

    Search for distance implemented

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -10,6 +10,18 @@ module.exports = {
             techs: {
                 $in: techsArray,
             },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [
+                            longitude,
+                            latitude
+                        ],
+                    },
+                    $maxDistance: 10000,
+                },
+            },
         })
         return response.json({ devs })
     }
