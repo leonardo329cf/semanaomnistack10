40df0df18eb7e683be6847177523630cf0504388
Leonardo Cardozo Ferreira <leonardo329cf@gmail.com>
2020-01-17 16:50:44 -0300

    Visual with mocked data implemented

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes'
+import { StatusBar } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-    </View>
+    <>
+    <StatusBar barStyle='light-content' backgroundColor='#7d40e7'/>
+    <Routes />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
