d835ecac8f83df6e33777d514171db33f4e42405
Leonardo Cardozo Ferreira <leonardo329cf@gmail.com>
2020-01-17 18:00:28 -0300

    Connection with backend implemented

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -3,8 +3,11 @@ import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'reac
 import MapView, { Marker, Callout } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons'
+import api from '../services/api'
 
 function Main({ navigation }) {
+    const [devs, setDevs] = useState([])
+    const [techs, setTechs] = useState('')
     const [currentRegion, setCurrentRegion] = useState(null)
     useEffect(() => {
 
@@ -30,40 +33,85 @@ function Main({ navigation }) {
         loadInitialPosition()
     }, [])
 
+    async function loadDevs() {
+        const { latitude, longitude } = currentRegion
+
+        const response = await api.get('/search', {
+            params: {
+                latitude,
+                longitude,
+                techs
+            }
+        })
+        console.log(response.data)
+        setDevs(response.data.devs)
+    }
+
+    function handleRegionChanged(region) {
+        console.log(region)
+        setCurrentRegion(region)
+
+    }
+
     if (!currentRegion) {
         return null;
     }
 
     return (
         <>
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -29.941759, longitude: -51.080085 }}>
-                <Image style={styles.avatar} source={{ uri: `https://avatars3.githubusercontent.com/u/39681326?s=460&v=4` }} />
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'leonardo329cf'})
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Leonardo Cardozo Ferreira</Text>
-                        <Text style={styles.devBio}>Estudante de Sistemas Embarcados. Em busca de aprender sobre as tenologias mais legais do momento.</Text>
-                        <Text style={styles.devTechs}>Java</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
-        <View style={styles.searchForm}>
-            <TextInput 
-            style={styles.searchInput}
-            placeholder="Buscar devs por techs..."
-            placeholderTextColor='#999'
-            autoCapitalize="words"
-            autoCorrect={false}
-            />
+            <MapView
+                onRegionChangeComplete={handleRegionChanged}
+                initialRegion={currentRegion}
+                style={styles.map}
+            >
+                {devs.map(dev => (
+                    <Marker
+                        key={dev._id}
+                        coordinate={{
+                            latitude: dev.location.coordinates[1],
+                            longitude: dev.location.coordinates[0]
+                        }}
+                        >
+                        <Image
+                            style={styles.avatar}
+                            source={{ uri: dev.avatar_url }} />
+                        <Callout onPress={() => {
+                            navigation.navigate('Profile', { github_username: dev.github_username })
+                        }}>
+                            <View
+                                style={styles.callout}>
+                                <Text
+                                    style={styles.devName}>{dev.name}</Text>
+                                <Text
+                                    style={styles.devBio}>{dev.bio}</Text>
+                                <Text
+                                    style={styles.devTechs}>{dev.techs.join(', ')}</Text>
+                            </View>
+                        </Callout>
+                    </Marker>
+                ))}
+            </MapView>
+            <View
+                style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por techs..."
+                    placeholderTextColor='#999'
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                    value={techs}
+                    onChangeText={setTechs}
+                />
 
-            <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
-                <MaterialIcons name="my-location" size={20} color="#fff" />
-            </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={loadDevs}
+                    style={styles.loadButton}>
+                    <MaterialIcons
+                        name="my-location" size={20}
+                        color="#fff" />
+                </TouchableOpacity>
 
-        </View>
+            </View>
         </>
     )
 }
@@ -117,7 +165,7 @@ const styles = StyleSheet.create({
         shadowColor: '#000',
         shadowOpacity: 0.2,
         shadowOffset: {
-            width:  4,
+            width: 4,
             height: 4,
         },
         elevation: 2,
